6566f8fcdcd8930f69698695929e475a748a2a23
Sujith-3517 <sujithsuji1471@gmail.com>
2023-12-24 20:35:24 +0530

    All test cases are passed

---
+++ Section 08 - Arrays in Java/S08L02 - Initialization on array in Java/src/main/java/org/studyeasy/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] names;
+        names = new String[10];
+        names[0] = "Chaand";
+        names[5] = "studyeasy";
+        System.out.println(names[5]);
+
+
+    }
+}
